e8cf0c7606f1106459a8dc5b2eca2192c0a8869d
Asmmund <antony.ermolenko@gmail.com>
2012-10-20 18:13:10 +0300

    saving on the last step added

--- app/controllers/user_signup_controller.rb
+++ app/controllers/user_signup_controller.rb
@@ -1,32 +1,37 @@
 class UserSignupController < ApplicationController
 
   def steps
-    @check = true
-    next_step
-    path = request.env['PATH_INFO']
-    @user = session[:user]     if path == '/step4'
-     render "user_signup/#{path[1..-1]}" if @check
+    @redir = true
+    path = request.env['PATH_INFO'][1..-1]
+    next_step path
+    if path == 'step4' && @redir
+      @user = User.create(session[:user])
+    end
+    render "user_signup/#{path}" if @redir
   end
   
-  def next_step
-    valid_or_back
+  def next_step path
+    valid_or_back path
     update
   end  
-  def valid_or_back
-    if params[:user].present?
-      params[:user].map do |k,v| 
-        if v == '' && @check
-          redirect_to :back, notice: 'Fill in the fields!' 
-          @check = false
-        end
-      end
-    else
-      redirect_to :back, notice: 'Fill in the fields!' 
-      @check = false 
-    end
- 
+  def valid_or_back path
+#    case path
+#    when 'step2'
+#        @redir =  false unless params[:user] && params[:user][:email].present?  
+#    when 'step3'
+#        @redir = false unless params[:user] && params[:user][:address].present?
+#    when 'step4'
+#        @redir = false unless params[:user] && params[:user][:company_info].present?
+#    end
+#      redirect_to :back, notice: 'Fill in the fields!'  unless @redir
+#
   end
+
   def update
-    session[:user].update_attributes(params[:user])
+    session[:user] = if session[:user].present?
+      session[:user].merge(params[:user]) { |key, old, new| new } 
+    else
+      params[:user]
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    session[:user] = params[:user]
+    session[:user] = {}
     redirect_to step2_path
   end
 end
