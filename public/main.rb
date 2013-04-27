6b08693bd71db171b28247df9151a97eaa61ba9b
Asmmund <antony.ermolenko@gmail.com>
2013-04-27 21:20:38 +0300

    [^] added some active_support features

--- public/main.rb
+++ public/main.rb
@@ -1,2 +1,9 @@
-require "./preeventualist"
-puts PreEventualist.search( 'truck' )
+# require "./preeventualist"
+# puts PreEventualist.search( 'truck' )
+require "active_support/all"
+array = %w(green violet purple, deathstar schtrosstruppen)
+p array
+params = { type: :text }
+defaults = { lang: :en }
+params.reverse_merge!(defaults).except! (:type)
+p params
